8b6bb1aa52191d296b4670a6a8717c7d34865a7f
Jacob-ham99 <ham20039@byui.edu>
2025-05-03 19:16:00 -0600

    fixing light not working

--- Mission/mission.js
+++ Mission/mission.js
@@ -6,10 +6,10 @@ selectElem.addEventListener('change', changeTheme);
 function changeTheme() {
   let current = selectElem.value;
 
-  if (current === 'dark') {
+  if (current == 'dark') {
     document.body.classList.add('dark');
     logo.src = 'byui-logo_white.png'; // update with your actual path to the white logo
-  } else if (current === 'light') {
+  } else (current == 'light') {
     document.body.classList.remove('dark');
     logo.src = 'byui-logo_blue.webp'; // original logo path
   }
